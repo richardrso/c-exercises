2a4e6edbffd784ab4181953939125b624ff6ce8f
rick <55769589+richardrso@users.noreply.github.com>
2023-10-13 15:47:33 -0300

    add input validation for user actions

--- simple-bank-console.c
+++ simple-bank-console.c
@@ -2,8 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct
-{
+typedef struct {
     char name[50];
     char email[50];
     char password[50];
@@ -13,152 +12,145 @@ typedef struct
 User currentUser;
 int isLoggedIn = 0;
 
-void registerUser()
-{
+void registerUser() {
     printf("Enter your name: ");
-    scanf("%s", currentUser.name);
+    while (scanf("%s", currentUser.name) != 1) {
+        printf("Invalid input. Please enter a valid name: ");
+        while(getchar() != '\n');
+    }
+    
     printf("Enter your email: ");
-    scanf("%s", currentUser.email);
+    while (scanf("%s", currentUser.email) != 1) {
+        printf("Invalid input. Please enter a valid email: ");
+        while(getchar() != '\n');
+    }
+    
     printf("Enter your password: ");
-    scanf("%s", currentUser.password);
+    while (scanf("%s", currentUser.password) != 1) {
+        printf("Invalid input. Please enter a valid password: ");
+        while(getchar() != '\n');
+    }
+    
     currentUser.balance = 0.0;
     printf("Registration Successful!\n");
 }
 
-void login()
-{
+void login() {
     char email[50], password[50];
 
     printf("Enter your email: ");
-    scanf("%s", email);
+    while (scanf("%s", email) != 1) {
+        printf("Invalid input. Please enter a valid email: ");
+        while(getchar() != '\n');
+    }
+    
     printf("Enter your password: ");
-    scanf("%s", password);
+    while (scanf("%s", password) != 1) {
+        printf("Invalid input. Please enter a valid password: ");
+        while(getchar() != '\n');
+    }
 
-    if (strcmp(email, currentUser.email) == 0 && strcmp(password, currentUser.password) == 0)
-    {
+    if (strcmp(email, currentUser.email) == 0 && strcmp(password, currentUser.password) == 0) {
         printf("Login Successful!\n");
         isLoggedIn = 1;
-    }
-    else
-    {
+    } else {
         printf("Invalid email or password!\n");
     }
 }
 
-void showBalance()
-{
-    if (isLoggedIn)
-    {
+void showBalance() {
+    if(isLoggedIn) {
         printf("Your balance is: %.2f\n", currentUser.balance);
-    }
-    else
-    {
+    } else {
         printf("You need to log in first.\n");
     }
 }
 
-void deposit()
-{
+void deposit() {
     float amount;
-
-    if (isLoggedIn)
-    {
+    
+    if(isLoggedIn) {
         printf("Enter amount to deposit: ");
-        scanf("%f", &amount);
-
-        if (amount < 0)
-        {
-            printf("Invalid amount. Please enter a positive number.\n");
+        while (scanf("%f", &amount) != 1 || amount < 0) {
+            printf("Invalid input. Please enter a positive number: ");
+            while(getchar() != '\n');
         }
-        else
-        {
-            currentUser.balance += amount;
-            printf("Deposit Successful!\n");
-        }
-    }
-    else
-    {
+        
+        currentUser.balance += amount;
+        printf("Deposit Successful!\n");
+        
+    } else {
         printf("You need to log in first.\n");
     }
 }
 
-void withdraw()
-{
+void withdraw() {
     float amount;
-
-    if (isLoggedIn)
-    {
+    
+    if(isLoggedIn) {
         printf("Enter amount to withdraw: ");
-        scanf("%f", &amount);
-
-        if (amount < 0)
-        {
-            printf("Invalid amount. Please enter a positive number.\n");
+        while (scanf("%f", &amount) != 1 || amount < 0 || amount > currentUser.balance) {
+            if(amount > currentUser.balance){
+                printf("Insufficient balance!\n");
+            }else{
+                printf("Invalid input. Please enter a positive number less than or equal to your balance: ");
+            }
+            while(getchar() != '\n');
         }
-        else if (amount <= currentUser.balance)
-        {
-            currentUser.balance -= amount;
-            printf("Withdrawal Successful!\n");
-        }
-        else
-        {
-            printf("Insufficient balance!\n");
-        }
-    }
-    else
-    {
+        
+        currentUser.balance -= amount;
+        printf("Withdrawal Successful!\n");
+        
+    } else {
         printf("You need to log in first.\n");
     }
 }
 
-int main()
-{
+int main() {
     int choice;
 
-    while (1)
-    {
+    while(1) {
         printf("\n1. Register\n2. Login\n");
-
-        if (isLoggedIn)
-        {
+        
+        if(isLoggedIn) {
             printf("3. Show Balance\n4. Deposit\n5. Withdraw\n6. Exit\n");
-        }
-        else
-        {
+        } else {
             printf("3. Exit\n");
         }
-
+        
         printf("Enter your choice: ");
-        scanf("%d", &choice);
-
-        switch (choice)
-        {
-        case 1:
-            registerUser();
-            break;
-        case 2:
-            login();
-            break;
-        case 3:
-            if (isLoggedIn)
-            {
-                showBalance();
-            }
-            else
-            {
-                exit(0);
-            }
-            break;
-        case 4:
-            deposit();
-            break;
-        case 5:
-            withdraw();
-            break;
-        case 6:
-            exit(0);
-        default:
+        
+        if(scanf("%d", &choice)!=1){
             printf("Invalid choice!\n");
+            while(getchar() != '\n');
+            continue;
+        }
+
+        switch(choice) {
+            case 1:
+                registerUser();
+                break;
+            case 2:
+                login();
+                break;
+            case 3:
+                if(isLoggedIn) {
+                    showBalance();
+                } else {
+                    exit(0);
+                }
+                break;
+            case 4:
+                deposit();
+                break;
+            case 5:
+                withdraw();
+                break;
+            case 6:
+                exit(0);
+            default:
+                printf("Invalid choice!\n");
+                while(getchar() != '\n');
         }
     }
 
