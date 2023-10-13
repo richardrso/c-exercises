fc44b44a67e4e502d87f9388e8d1aa6cd246f5d0
rick <55769589+richardrso@users.noreply.github.com>
2023-10-13 15:32:29 -0300

    fix bug with negative deposit and withdrawal amounts

--- simple-bank-console.c
+++ simple-bank-console.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
+typedef struct
+{
     char name[50];
     char email[50];
     char password[50];
@@ -12,7 +13,8 @@ typedef struct {
 User currentUser;
 int isLoggedIn = 0;
 
-void registerUser() {
+void registerUser()
+{
     printf("Enter your name: ");
     scanf("%s", currentUser.name);
     printf("Enter your email: ");
@@ -23,7 +25,8 @@ void registerUser() {
     printf("Registration Successful!\n");
 }
 
-void login() {
+void login()
+{
     char email[50], password[50];
 
     printf("Enter your email: ");
@@ -31,91 +34,131 @@ void login() {
     printf("Enter your password: ");
     scanf("%s", password);
 
-    if (strcmp(email, currentUser.email) == 0 && strcmp(password, currentUser.password) == 0) {
+    if (strcmp(email, currentUser.email) == 0 && strcmp(password, currentUser.password) == 0)
+    {
         printf("Login Successful!\n");
-        isLoggedIn = 1; 
-    } else {
+        isLoggedIn = 1;
+    }
+    else
+    {
         printf("Invalid email or password!\n");
     }
 }
 
-void showBalance() {
-    if(isLoggedIn) { 
+void showBalance()
+{
+    if (isLoggedIn)
+    {
         printf("Your balance is: %.2f\n", currentUser.balance);
-    } else {
+    }
+    else
+    {
         printf("You need to log in first.\n");
     }
 }
 
-void deposit() {
+void deposit()
+{
     float amount;
-    
-    if(isLoggedIn) { 
+
+    if (isLoggedIn)
+    {
         printf("Enter amount to deposit: ");
         scanf("%f", &amount);
-        currentUser.balance += amount;
-        printf("Deposit Successful!\n");
-    } else {
+
+        if (amount < 0)
+        {
+            printf("Invalid amount. Please enter a positive number.\n");
+        }
+        else
+        {
+            currentUser.balance += amount;
+            printf("Deposit Successful!\n");
+        }
+    }
+    else
+    {
         printf("You need to log in first.\n");
     }
 }
 
-void withdraw() {
+void withdraw()
+{
     float amount;
-    
-    if(isLoggedIn) {
+
+    if (isLoggedIn)
+    {
         printf("Enter amount to withdraw: ");
         scanf("%f", &amount);
-        if (amount <= currentUser.balance) {
+
+        if (amount < 0)
+        {
+            printf("Invalid amount. Please enter a positive number.\n");
+        }
+        else if (amount <= currentUser.balance)
+        {
             currentUser.balance -= amount;
             printf("Withdrawal Successful!\n");
-        } else {
+        }
+        else
+        {
             printf("Insufficient balance!\n");
         }
-    } else {
+    }
+    else
+    {
         printf("You need to log in first.\n");
     }
 }
 
-int main() {
+int main()
+{
     int choice;
 
-    while(1) {
+    while (1)
+    {
         printf("\n1. Register\n2. Login\n");
-        
-        if(isLoggedIn) {
+
+        if (isLoggedIn)
+        {
             printf("3. Show Balance\n4. Deposit\n5. Withdraw\n6. Exit\n");
-        } else {
+        }
+        else
+        {
             printf("3. Exit\n");
         }
-        
+
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch(choice) {
-            case 1:
-                registerUser();
-                break;
-            case 2:
-                login();
-                break;
-            case 3:
-                if(isLoggedIn) {
-                    showBalance();
-                } else {
-                    exit(0);
-                }
-                break;
-            case 4:
-                deposit();
-                break;
-            case 5:
-                withdraw();
-                break;
-            case 6:
+        switch (choice)
+        {
+        case 1:
+            registerUser();
+            break;
+        case 2:
+            login();
+            break;
+        case 3:
+            if (isLoggedIn)
+            {
+                showBalance();
+            }
+            else
+            {
                 exit(0);
-            default:
-                printf("Invalid choice!\n");
+            }
+            break;
+        case 4:
+            deposit();
+            break;
+        case 5:
+            withdraw();
+            break;
+        case 6:
+            exit(0);
+        default:
+            printf("Invalid choice!\n");
         }
     }
 
